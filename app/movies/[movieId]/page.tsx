cb9047534701cfda168c6433bafabfa5e43bc23f
ParkYoungWoong <thesecon@gmail.com>
2026-08-28 16:42:49 +0900

    260828-16:42

--- app/movies/[movieId]/page.tsx
+++ app/movies/[movieId]/page.tsx
@@ -1,3 +1,23 @@
-export default async function MovieDetailsPage() {
+interface Props {
+  params: Promise<{
+    movieId: string
+  }>
+}
+
+export default async function MovieDetailsPage({ params }: Props) {
+  const { movieId } = await params
+  await fetch(
+    `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&i=${movieId}`
+  )
+
   return <></>
 }
+
+// --- await promise를 이해하기 위한 예제 ---
+//
+// async function add(): Promise<number> {
+//   return 123
+// }
+// const 객체 = add()
+// const 결과 = await 객체
+// console.log(결과) // 123
