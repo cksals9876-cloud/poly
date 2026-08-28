340b497369c5add7f143eb7234ec7454868546ef
ParkYoungWoong <thesecon@gmail.com>
2026-08-28 15:49:53 +0900

    260828-15:49

--- app/movies/page.tsx
+++ app/movies/page.tsx
@@ -1,12 +1,38 @@
+'use client'
+import { useState } from 'react'
 import { fetchMovies } from '@/serverActions'
+import type { Movie } from '@/serverActions'
+import Link from 'next/link'
 
-export default async function MoviesPage() {
+export default function MoviesPage() {
+  const [movies, setMovies] = useState<Movie[]>([])
+
+  async function handleSubmit(event: React.SubmitEvent<HTMLFormElement>) {
+    event.preventDefault()
+    const formData = new FormData(event.currentTarget)
+    const searchText = formData.get('searchText') as string
+    const movies = await fetchMovies(searchText)
+    console.log(movies) // 영화 목록?!
+    setMovies(movies)
+  }
   return (
     <>
-      <form action={fetchMovies}>
-        <input type="text" />
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          name="searchText"
+        />
         <button type="submit">검색</button>
       </form>
+      <ul>
+        {movies.map(movie => {
+          return (
+            <li key={movie.imdbID}>
+              <Link href={`/movies/${movie.imdbID}`}>{movie.Title}</Link>
+            </li>
+          )
+        })}
+      </ul>
     </>
   )
 }
--- serverActions/index.ts
+++ serverActions/index.ts
@@ -1,9 +1,15 @@
 'use server'
 
-export async function fetchMovies() {
+export interface Movie {
+  imdbID: string
+  Title: string
+  Poster: string
+}
+
+export async function fetchMovies(searchText: string) {
   const res = await fetch(
-    `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=spider`
+    `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&s=${searchText}`
   )
   const data = await res.json()
-  return data.Search
+  return (data.Search || []) as Movie[]
 }
