faaa5d21b2b64ebe0d17a0e0c0b243b2f62b7856
pakrchanmin <cksals9876@gmail.com>
2026-09-10 12:34:17 +0900

    260910-12:34

--- app/(header)/movies/[movieId]/page.tsx
+++ app/(header)/movies/[movieId]/page.tsx
@@ -11,6 +11,35 @@ interface Props {
   searchParams: Promise<SP>
 }
 
+async function fetchMovie(movieId: string, plot: string) {
+  const res = await fetch(
+    // `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&i=${movieId}&plot=${plot}`
+    `${process.env.NEXT_PUBLIC_URL}/api/movies/${movieId}?plot=${plot}`,
+    {
+      cache: 'force-cache'
+    }
+  )
+  const movie: Movie = await res.json()
+  return movie
+}
+
+export async function generateMetadata({ params, searchParams }: Props) {
+  const { movieId } = await params
+  const { plot = 'short' }: SP = await searchParams
+  // await new Promise(resolve => setTimeout(resolve, 2000))
+  const movie = await fetchMovie(movieId, plot)
+
+  return {
+    openGraph: {
+      type: 'website',
+      siteName: 'Next.js 연습 프로젝트',
+      title: movie.Title,
+      description: movie.Plot,
+      images: movie.Poster
+    }
+  }
+}
+
 // http://localhost:3000/movies/tt1234567890?plot=full
 export default async function MovieDetailsPage({
   params,
@@ -19,12 +48,7 @@ export default async function MovieDetailsPage({
   const { movieId } = await params
   const { plot = 'short' }: SP = await searchParams
   // await new Promise(resolve => setTimeout(resolve, 2000))
-  const res = await fetch(
-    // `https://omdbapi.com?apikey=${process.env.OMDB_API_KEY}&i=${movieId}&plot=${plot}`
-    `${process.env.NEXT_PUBLIC_URL}/api/movies/${movieId}?plot=${plot}`
-  )
-  const movie: Movie = await res.json()
-
+  const movie = await fetchMovie(movieId, plot)
   // --- ❌ 에러 발생 예시 ---
   // const myError = new Error(
   //   '영화 상세 정보를 가져오다가 알 수 없는 문제가 발생했습니다. 나중에 다시 시도해 보세요!'
